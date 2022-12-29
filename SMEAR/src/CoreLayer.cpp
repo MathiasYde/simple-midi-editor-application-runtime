ff76d1add053e9163aba053f93130be60e7f1dc1
Mathias <mathias.yde@hotmail.dk>
2022-12-29 22:05:30 +0100

    Removed redundant code from CoreLayer

--- SMEAR/src/CoreLayer.cpp
+++ SMEAR/src/CoreLayer.cpp
@@ -27,13 +27,6 @@ namespace SMEAR {
 	void CoreLayer::OnUpdate(TimeStep ts) {
 		libremidi::message message;
 		if (m_MidiHandle.get_message(message)) {
-
-			uint8_t command = message[0] >> 4;
-			uint8_t channel = message[0] & 0x0F;
-			uint8_t pitch = message[1] & 0x7F;
-			uint8_t velocity = message[2] & 0x7F;
-
-			NT_INFO("Midi event - Command {0}, Channel {1}, Pitch {2}, Velocity {3}", std::to_string(command), std::to_string(channel), std::to_string(pitch), std::to_string(velocity));
 			uint8_t command = message[0] & 0xF0;
 			switch (command) {
 			case MIDI_NOTE_ON: {
